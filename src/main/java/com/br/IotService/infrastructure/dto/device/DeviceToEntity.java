b52a7702ed55b80183e5644bd9fc0bf846602b4b
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-12 11:39:10 -0300

    INFRASTRUCTURE - Device salvando com o createdAt

--- src/main/java/com/br/IotService/infrastructure/dto/device/DeviceToEntity.java
+++ src/main/java/com/br/IotService/infrastructure/dto/device/DeviceToEntity.java
@@ -3,6 +3,8 @@ package com.br.IotService.infrastructure.dto.device;
 import com.br.IotService.core.domain.Device;
 import com.br.IotService.infrastructure.entity.DeviceEntity;
 
+import java.time.LocalDateTime;
+
 public record DeviceToEntity(Device device) {
 
     public DeviceEntity toEntity(){
@@ -12,8 +14,8 @@ public record DeviceToEntity(Device device) {
                 .type(device.getType())
                 .status(device.getStatus())
                 .location(device.getLocation())
-                .createdAt(device.getCreatedAt())
-                .lastSeen(device.getLastSeen())
+                .createdAt(LocalDateTime.now())
+                .lastSeen(LocalDateTime.now())
                 .build();
     }
 }
