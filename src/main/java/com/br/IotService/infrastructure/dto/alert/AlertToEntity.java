25dd81b870e237cd235f01e6ad078ff1f59fece2
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-12 13:57:18 -0300

    INFRASTRUCTURE - AlertToEntity com o timestamp LocalDateTime.now()

--- src/main/java/com/br/IotService/infrastructure/dto/alert/AlertToEntity.java
+++ src/main/java/com/br/IotService/infrastructure/dto/alert/AlertToEntity.java
@@ -4,6 +4,8 @@ import com.br.IotService.core.domain.Alert;
 import com.br.IotService.infrastructure.dto.device.DeviceToEntity;
 import com.br.IotService.infrastructure.entity.AlertEntity;
 
+import java.time.LocalDateTime;
+
 public record AlertToEntity(Alert alert) {
 
     public AlertEntity toAlertEntity(){
@@ -11,7 +13,7 @@ public record AlertToEntity(Alert alert) {
                 .builder()
                 .id(alert.getId())
                 .deviceEntity(new DeviceToEntity(alert.getDevice()).toEntity())
-                .timestamp(alert.getTimestamp())
+                .timestamp(LocalDateTime.now())
                 .severity(alert.getSeverity())
                 .description(alert.getDescription())
                 .build();
